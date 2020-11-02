3686e3f554cf5648cf6c5290017d59a5059c2a06
Shivank254 <71582134+Shivank254@users.noreply.github.com>
2020-11-02 20:19:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-// wil add things here later projects
+
+function add_user(){
+
+    user_login_username = document.getElementById("username").value;
+
+    if( user_login_username == ""){
+
+        alert("Please enter your username to get registered");
+
+    }
+    else{
+
+        localStorage.setItem("username_key", user_login_username);
+
+        window.location = "kwitter_room.html";
+    }
+
+}
